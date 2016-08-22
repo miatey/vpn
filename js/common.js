8cdf3c9a5a0472c5fe5244fed524c5549081c86b
miatey <troyan@ukr.net>
2016-08-22 10:30:28 +0300

    vpn

---
+++ js/common.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+	$('.btn-nav').on('click', function(){
+			var menu = $('.header__nav');
+			menu.toggleClass('open-nav')
+		});
+
+	$('.js-slick').slick({
+	  dots: true,
+	  infinite: true,
+	  speed: 300,
+	  slidesToShow: 1,
+		slidesToScroll: 1,
+	  arrows: false,
+  	autoplay: true,
+  	autoplaySpeed: 2000,
+	});
+});
---
+++ js/init.js
@@ -0,0 +1,4 @@
+head.load("js/lib/jquery.js",
+					"js/lib/slick.min.js",
+					"js/lib/cycle.js",
+					"js/common.js");
